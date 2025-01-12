685ad10e0f450099a8f520f104793517cc89ecc6
Srinivas <vasus4990@gmail.com>
2025-01-12 19:09:38 +0530

    Gemini Chat Bot added

--- src/app/(root)/_components/ClarifyButton.tsx
+++ src/app/(root)/_components/ClarifyButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from "react";
+import React, { JSX } from "react";
 import { ButtonsCard } from "@/components/ui/tailwindcss-buttons";
 
 interface ClarifyButtonProps {
